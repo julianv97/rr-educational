06d9af60e33c6ed7c312c04576b2df6bc46c196c
julianv97 <julian.vicente@radiumrocket.com>
2022-08-14 01:57:50 -0300

    RRE: create auth store

--- src/screens/Auth/Login/index.tsx
+++ src/screens/Auth/Login/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import ButtonSocialAuth from '../../../components/ButtonSocialAuth';
 import styles from './styles';
 
+import useBearStore from '../../../stores/auth';
+import useGoogleAuth from '../../../hooks/useGoogleAuth';
+
 const Login = () => {
+  const { response, promptAsync } = useGoogleAuth();
+  const { login, logout, isLoggedIn, currentUser } = useBearStore();
+
   const handleGoogleAuth = () => {
-    console.log('handleGoogleAuth');
+    login(response, promptAsync);
   };
+
+  console.log('isLoggedIn', isLoggedIn);
+  console.log('currentUser', currentUser);
 
   return (
     <View style={styles.container}>
       <ButtonSocialAuth name="google" title="Login with Google" onPress={handleGoogleAuth} />
+      <Button title="Login with Facebook" onPress={logout} />
     </View>
   );
 };
---
+++ src/stores/auth.ts
@@ -0,0 +1,46 @@
+import create from 'zustand';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { AuthRequestPromptOptions, AuthSessionResult } from 'expo-auth-session';
+import { auth } from '../helpers/firebase';
+
+interface AuthState {
+  isLoggedIn: boolean;
+  currentUser: string;
+  login: (
+    response: AuthSessionResult | null,
+    promptAsync: (options?: AuthRequestPromptOptions | undefined) => Promise<AuthSessionResult>,
+  ) => void;
+  logout: any;
+}
+
+const useAuthState = create<AuthState>((set) => ({
+  isLoggedIn: false,
+  currentUser: '',
+
+  login: async (response, promptAsync) => {
+    await promptAsync({ useProxy: true });
+    if (response?.type !== 'success') return;
+    const { id_token: idToken } = response.params;
+    const credential = GoogleAuthProvider.credential(idToken);
+    const { user } = await auth.signInWithCredential(credential);
+    console.log(user);
+    set({
+      isLoggedIn: true,
+      currentUser: user?.displayName!,
+    });
+  },
+
+  logout: async () => {
+    await auth.signOut();
+    set({
+      isLoggedIn: false,
+      currentUser: '',
+    });
+  },
+}));
+
+export default useAuthState;
+
+/* const logOut = () => {
+    auth.signOut();
+  }; */
